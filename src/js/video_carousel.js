4e7288029063e1687d3850045b232af3065deee4
Jakub Klapka <klapka@lumiart.cz>
2014-11-07 14:15:23 +0100

    Add home video functions

---
+++ src/js/video_carousel.js
@@ -0,0 +1,58 @@
+/* global jQuery */
+( function( $ ){
+
+	var VideoCarousel = {
+
+		init: function( el ) {
+
+			this.wrap = el;
+			this.main_video = this.wrap.find( '[data-video-carousel-main_video]' );
+			this.items = this.wrap.find( '[data-video-carousel-item]' );
+			this.video_template = this.wrap.find( '[data-video-carousel-template]' ).html();
+			this.current_item = this.items.eq(0);
+
+			this.bindEvents();
+		},
+
+		bindEvents: function() {
+
+			var self = this;
+			this.items.on( 'click', function( e ){
+				e.preventDefault();
+				self.itemClicked( $( this ) );
+			} );
+
+		},
+
+		itemClicked: function( el ) {
+			if( el.is( this.current_item ) ) {
+				return;
+			}
+
+			this.changeVideo( el );
+		},
+
+		changeVideo: function( el ) {
+
+			this.current_item = el;
+			var yt_id = el.data( 'yt-id' );
+
+			this.items.removeClass( 'is-active' );
+			el.addClass( 'is-active' );
+
+			var video = this.video_template.replace( '[[YT_ID]]', yt_id );
+			this.main_video.html( video );
+
+		}
+
+	};
+
+	$( function(){
+
+		$( '[data-video-carousel]' ).each( function() {
+			Object.create( VideoCarousel ).init( $( this ) );
+		} );
+
+	} );
+
+} )( jQuery );
